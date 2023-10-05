21d132e69dcc3a1ac68393241627fe5e2f913dbb
A7med-Magdy <ahmed.magdy12mohammed@gmail.com>
2023-10-05 15:26:02 -0700

    New function for print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program prints a quote using write function
+ *
+ * Return: 1 (error)
+*/
+
+int main(void)
+{	char quote[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, 59);
+	return (1);
+}
