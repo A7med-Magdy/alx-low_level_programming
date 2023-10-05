220499ff93f0c86a355b448615e4cb335e6558bf
A7med-Magdy <ahmed.magdy12mohammed@gmail.com>
2023-10-05 15:29:40 -0700

    New function for print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 */
 
 int main(void)
-{	char quote[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quote[60];
+	quote[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quote, 59);
 	return (1);
 }
