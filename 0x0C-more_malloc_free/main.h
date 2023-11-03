7e9da6dbfef9767b71502f15531516907a852678
A7med-Magdy <ahmed.magdy12mohammed@gmail.com>
2023-11-02 18:48:55 -0700

    Header_file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
